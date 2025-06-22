be350c0d6fb4a64ef90407cc7fc5dce2b7fe71a9
Liow Zhi Heng <liowzhiheng@gmail.com>
2025-06-22 22:50:53 +0800

    Basic setup

---
+++ health-fitness-hub/src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+// src/environments/environment.development.ts
+export const environment = {
+  production: false,
+  rapidApiKey: 'YOUR_RAPIDAPI_KEY_HERE' // <-- PASTE YOUR KEY
+};
---
+++ health-fitness-hub/src/environments/environment.ts
@@ -0,0 +1,5 @@
+// src/environments/environment.ts
+export const environment = {
+  production: true,
+  rapidApiKey: '' // It's best practice to use a different key for production
+};
